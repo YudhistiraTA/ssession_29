3499dbb1712a4a80586c33aadc2872b8f13c7106
Yudhistira Tribuana <yudhistira.tribuana@gmail.com>
2025-08-06 01:43:38 +0700

    styling

--- mvc/main.go
+++ mvc/main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"net/http"
 
 	"binus.ac.id/controller"
 )
 
+//go:embed static
+var static embed.FS
+
 // Main Application
 func main() {
-	// Configure routes
 	http.HandleFunc("/", controller.HomeController)
 	http.HandleFunc("/echo", controller.EchoController)
 
-	// Start server
+	http.Handle("/static/", http.FileServer(http.FS(static)))
+
 	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
